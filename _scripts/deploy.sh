31968c9bbd4117e69eb4cdf9e9e6d63a50423567
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2020-06-26 11:36:23 -0400

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -16,7 +16,7 @@ SITE_DIR="/var/www/link.hypha.coop"
 # This is done to skip the fingerprint check on new connection
 # - Skip the host fingerprint check
 # - Don't save the fingerprint to known_hosts
-rsync -v -r 
+rsync -v -r \
   --delete-after 
   -e "ssh -i $IDENTITY_FILE -o StrictHostKeyChecking=no -p $PORT" \
   _site/* $USER@$SERVER:$SITE_DIR || true
