9b8edbc71f8bd08323f04828fc4dbf05f3ed8a1e
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2020-06-26 11:24:14 -0400

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -16,6 +16,7 @@ SITE_DIR="/var/www/link.hypha.coop"
 # This is done to skip the fingerprint check on new connection
 # - Skip the host fingerprint check
 # - Don't save the fingerprint to known_hosts
+ls -la
 scp -r \
   -i $IDENTITY_FILE \
   -P $PORT \
