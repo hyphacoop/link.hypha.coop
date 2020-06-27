9ee0f6ed8858e8f433ce100a98907e2b65cc4f82
dev@aseriesoftubez.com <dev@aseriesoftubez.com>
2020-06-26 23:44:40 -0400

    Travis script fixes

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -11,7 +11,7 @@ USER="${1:-hyphacoop}"
 IDENTITY_FILE="${2:-_scripts/id_rsa}"
 SERVER="${3:-hypha.coop}"
 PORT="${4:-22}"
-SITE_DIR="/var/www/link.hypha.coop"
+SITE_DIR="${5:-/var/www/html}"
 
 # This is done to skip the fingerprint check on new connection
 # - Skip the host fingerprint check
